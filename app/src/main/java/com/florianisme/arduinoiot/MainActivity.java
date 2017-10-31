0a7bc021660e68f80088f6bbd7856611f7fa3cbe
Florian <florian.m9782@gmail.com>
2017-10-31 17:57:19 +0100

    Added MainActivity layout

---
+++ app/src/main/java/com/florianisme/arduinoiot/MainActivity.java
@@ -0,0 +1,12 @@
+package com.florianisme.arduinoiot;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Florian Möhle on 31.10.2017
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
